8b5e6594fdc01a7f906464261d43b18586cfb776
木子 <luito@qq.com>
2016-03-03 17:15:01 +0800

    11

--- Hyde.Web/js/product.js
+++ Hyde.Web/js/product.js
@@ -1,5 +1,13 @@
-﻿$(function () {
+﻿var myScroll,
+	pullDownEl, pullDownOffset,
+	pullUpEl, pullUpOffset,
+	generatedCount = 0;
+$(function () {
     getBrandLst();
+    //初始化绑定iScroll控件 
+    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+    setTimeout(loaded, 200);
 });
 /**
  * 将数值四舍五入(保留2位小数)后格式化成金额形式
@@ -119,3 +127,89 @@ function getBrandLst() {
         }
     });
 }
+
+
+/**
+ * 下拉刷新 （自定义实现此方法）
+ * myScroll.refresh();		// 数据加载完成后，调用界面更新方法
+ */
+function pullDownAction() {
+    setTimeout(function () {	// <-- Simulate network congestion, remove setTimeout from production!
+        searchGo();
+        myScroll.refresh();		//数据加载完成后，调用界面更新方法   Remember to refresh when contents are loaded (ie: on ajax completion)
+    }, 1000);	// <-- Simulate network congestion, remove setTimeout from production!
+}
+
+/**
+ * 滚动翻页 （自定义实现此方法）
+ * myScroll.refresh();		// 数据加载完成后，调用界面更新方法
+ */
+function pullUpAction() {
+    setTimeout(function () {	// <-- Simulate network congestion, remove setTimeout from production!
+        //var el, li, i;
+        //el = document.getElementById('thelist');
+
+        //for (i = 0; i < 3; i++) {
+        //    li = document.createElement('li');
+        //    li.innerText = 'Generated row ' + (++generatedCount);
+        //    el.appendChild(li, el.childNodes[0]);
+        //}
+
+        myScroll.refresh();		// 数据加载完成后，调用界面更新方法 Remember to refresh when contents are loaded (ie: on ajax completion)
+    }, 1000);	// <-- Simulate network congestion, remove setTimeout from production!
+}
+/**
+ * 初始化iScroll控件
+ */
+function loaded() {
+    pullDownEl = document.getElementById('pullDown');
+    pullDownOffset = pullDownEl.offsetHeight;
+    pullUpEl = document.getElementById('pullUp');
+    pullUpOffset = pullUpEl.offsetHeight;
+
+    myScroll = new iScroll('wrapper', {
+        useTransition: true,
+        topOffset: pullDownOffset,
+        onRefresh: function () {
+            if (pullDownEl.className.match('loading')) {
+                pullDownEl.className = '';
+                pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Pull down to refresh...';
+            } else if (pullUpEl.className.match('loading')) {
+                pullUpEl.className = '';
+                pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Pull up to load more...';
+            }
+        },
+        onScrollMove: function () {
+            if (this.y > 5 && !pullDownEl.className.match('flip')) {
+                pullDownEl.className = 'flip';
+                pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Release to refresh...';
+                this.minScrollY = 0;
+            } else if (this.y < 5 && pullDownEl.className.match('flip')) {
+                pullDownEl.className = '';
+                pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Pull down to refresh...';
+                this.minScrollY = -pullDownOffset;
+            } else if (this.y < (this.maxScrollY - 5) && !pullUpEl.className.match('flip')) {
+                pullUpEl.className = 'flip';
+                pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Release to refresh...';
+                this.maxScrollY = this.maxScrollY;
+            } else if (this.y > (this.maxScrollY + 5) && pullUpEl.className.match('flip')) {
+                pullUpEl.className = '';
+                pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Pull up to load more...';
+                this.maxScrollY = pullUpOffset;
+            }
+        },
+        onScrollEnd: function () {
+            if (pullDownEl.className.match('flip')) {
+                pullDownEl.className = 'loading';
+                pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Loading...';
+                pullDownAction();	// Execute custom function (ajax call?)
+            } else if (pullUpEl.className.match('flip')) {
+                pullUpEl.className = 'loading';
+                pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Loading...';
+                pullUpAction();	// Execute custom function (ajax call?)
+            }
+        }
+    });
+
+    setTimeout(function () { document.getElementById('wrapper').style.left = '0'; }, 800);
+}
